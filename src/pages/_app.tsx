8ef987fba7bb0a4e2b6644b54b33c4e3772dffa8
Nicholas Romero <ncrmro@gmail.com>
2022-11-27 09:05:11 -0600

    feat: add layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,30 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "~/lensAPI";
 
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+function Layout(props: { children: React.ReactElement }) {
+  return (
+    <>
+      <nav id="navbar" className="flex justify-items-center w-full">
+        <Link href="/">John Doe</Link>
+      </nav>
+      <main>{props.children}</main>
+    </>
+  );
+}
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ApolloProvider>
   );
 }
