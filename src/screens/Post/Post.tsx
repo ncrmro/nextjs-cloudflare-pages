473194c79012b13120bd381858134caf2edf3dc2
Nicholas Romero <ncrmro@gmail.com>
2022-11-27 20:53:37 -0600

    feat(web): add initial post edit

--- src/screens/Post/Post.tsx
+++ src/screens/Post/Post.tsx
@@ -3,16 +3,41 @@ import {
   usePostScreenQuery,
   PostScreenPostFragment,
 } from "~/screens/Post/Post.generated";
+import PostForm, { usePostFormState } from "~/components/PostForm";
+
+function PostScreenEdit(post: PostScreenPostFragment) {
+  const [state, setState] = usePostFormState({
+    title: post?.metadata.name,
+    content: post?.metadata.description,
+  });
+  return <PostForm state={state} setState={setState} />;
+}
 
 export default function PostScreen() {
   const router = useRouter();
-  const { id } = router.query;
-  const { data } = usePostScreenQuery({ variables: { publicationId: id } });
+  const isEdit = router.query.edit;
+  const { data, loading } = usePostScreenQuery({
+    variables: { publicationId: router.query.id },
+  });
   const post = data?.publication as PostScreenPostFragment;
+  const toggleEdit = async () => {
+    let url = `/posts/${router.query.id}`;
+    if (!router.query.edit) {
+      url = `${url}?edit=true`;
+    }
+    await router.push(url);
+  };
   return (
     <>
-      <h1>{post?.metadata.name}</h1>
-      <div>{post?.metadata.description}</div>
+      <div>
+        <h1>{post?.metadata.name}</h1>
+        <button onClick={toggleEdit}>{isEdit ? "Close Edit" : "Edit"}</button>
+      </div>
+      {isEdit && !loading ? (
+        <PostScreenEdit {...post} />
+      ) : (
+        <div>{post?.metadata.description}</div>
+      )}
     </>
   );
 }
